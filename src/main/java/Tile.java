86b2d4e999004cbf43ec60285af73d09dcea9680
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-28 23:11:22 +0300

    Basics of Tile Class

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,5 +1,6 @@
 public class Tile {
     private int no;
+    private static final int COLOR_PER_COLOR = 13;
     enum COLOR{
         YELLOW,
         BLUE,
@@ -8,4 +9,22 @@ public class Tile {
         FAKE_JOKER
     }
 
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+    public COLOR getColor() throws WrongColorException{
+        int colorNo = no / COLOR_PER_COLOR;
+        return switch (colorNo) {
+            case 0 -> COLOR.YELLOW;
+            case 1 -> COLOR.BLUE;
+            case 2 -> COLOR.BLACK;
+            case 3 -> COLOR.RED;
+            default -> throw new WrongColorException(no);
+        };
+
+    }
 }
