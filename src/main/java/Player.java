30438715f73a02c4ea08a397dbe652036d97c45b
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-29 13:34:12 +0300

    Board Created for Players.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,8 +1,18 @@
 import java.util.LinkedList;
 
 public class Player {
-    private LinkedList<Integer> board;
+    private LinkedList<Tile> board;
+    //private Tile left; //for future purposes
+    //private Tile right; //for future purposes
     Player(){
+        board = new LinkedList<>();
+    }
 
+    public LinkedList<Tile> getBoard() {
+        return board;
+    }
+
+    public void setBoard(LinkedList<Tile> board) {
+        this.board = board;
     }
 }
