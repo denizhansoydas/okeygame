37bb237e02c494b56dc8bbb375e179de263efb01
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-29 13:50:45 +0300

    Now the Tiles are stored in an array list instead of an array.
    Now the game itself shuffles all the tiles.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,36 +2,45 @@ import java.util.*;
 
 public class Game {
     //static properties and methods
-    public static final int TILE_COUNT = 106;
+    public static final int START_TILE_COUNT = 106;
     public static final int PLAYER_COUNT = 4;
 
     //properties
-    private Tile[] tiles;
+
+    private ArrayList<Tile> shuffledTiles;
     private Player[] players;
     //constructors
     Game(){
-        tiles = new Tile[TILE_COUNT];
-        for(int i = 0; i < TILE_COUNT / 2; i++){
+        Tile[] tiles;
+        shuffledTiles = new ArrayList<>();
+        players = new Player[PLAYER_COUNT];
+
+        tiles = new Tile[START_TILE_COUNT];
+        for(int i = 0; i < START_TILE_COUNT / 2; i++){
             //There are two instances of a tile. Therefore, the array will be like: [0,...52] [0,...,52]
             tiles[i] = new Tile(i);
-            tiles[TILE_COUNT / 2 + i] = new Tile(i);
+            tiles[START_TILE_COUNT / 2 + i] = new Tile(i);
         }
-        players = new Player[PLAYER_COUNT];
+
         for(int i = 0; i < PLAYER_COUNT; i++)
             players[i] = new Player();
+        shuffle(tiles);
+        Collections.addAll(shuffledTiles, tiles);
     }
 
-    public Tile[] getTiles() {
-        return tiles;
-    }
-
-    public void setTiles(Tile[] tiles) {
-        this.tiles = tiles;
-    }
-    public void shuffle(){
+    //Method to shuffle the tiles. Aimed to be used only in constructor.
+    private void shuffle(Tile[] tiles){
         List<Tile> tiles_temp = Arrays.asList(tiles);
         Collections.shuffle(tiles_temp);
         tiles_temp.toArray(tiles);
+    }
+
+    public ArrayList<Tile> getShuffledTiles() {
+        return shuffledTiles;
+    }
+
+    public void setShuffledTiles(ArrayList<Tile> shuffledTiles) {
+        this.shuffledTiles = shuffledTiles;
     }
 
     public Player[] getPlayers() {
