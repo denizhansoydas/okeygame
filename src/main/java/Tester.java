ac05edb5f8e04362a1212106ade78d08bb6f3828
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-29 13:12:28 +0300

    Updated according to the recycled Tile Class.

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -6,13 +6,13 @@
 public class Tester {
     public static void main(String[] args) {
         Game game = new Game();
-        Integer[] tiles = game.getTiles();
-        for(Integer i: tiles)
-            System.out.print(i + " ");
+        Tile[] tiles = game.getTiles();
+        for(Tile i: tiles)
+            System.out.println(i + " ");
         System.out.println();
         game.shuffle();
         System.out.println("Shuffled.");
-        for(Integer i: tiles)
-            System.out.print(i + " ");
+        for(Tile i: tiles)
+            System.out.println(i + " ");
     }
 }
