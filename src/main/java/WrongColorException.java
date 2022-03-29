8206e76d5b40d41d73e6c0d05b5fc7c52e4e8c74
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-29 13:04:47 +0300

    Exception Time Changed. It is not a Checked Exception anymore.

--- src/main/java/WrongColorException.java
+++ src/main/java/WrongColorException.java
@@ -1,4 +1,4 @@
-public class WrongColorException extends Exception{
+public class WrongColorException extends RuntimeException{
     WrongColorException(int number){
         System.err.println("Wrong coloring with number: " + number);
     }
