02597c3638cf0c56d731a11fe5cd854a6851cde4
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-29 13:11:45 +0300

    Class recycled.
    some static methods changed to instance methods.

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,25 +1,12 @@
 import java.util.Arrays;
 
 /**
- * This class is deprecated.
+ * @author Denizhan Soydas
+ * @version 1.0-SNAPSHOT
  */
 public class Tile{
     //static properties, methods and enumerations
     public static final int TILE_PER_COLOR = 13;
-
-    public static COLOR getColor(int no) throws WrongColorException{
-        if (no == maxNumber())
-            return COLOR.FAKE_JOKER;
-        int colorNo = no / TILE_PER_COLOR;
-
-        return switch (colorNo) {
-            case 0 -> COLOR.YELLOW;
-            case 1 -> COLOR.BLUE;
-            case 2 -> COLOR.BLACK;
-            case 3 -> COLOR.RED;
-            default -> throw new WrongColorException(no);
-        };
-    }
     public static int maxNumber(){
         return TILE_PER_COLOR * (COLOR.values().length - 1);
     }
@@ -51,15 +38,28 @@ public class Tile{
     public int getNo() {
         return no;
     }
-    public int getValue(){
-        return no % TILE_PER_COLOR + 1;
-    }
     public void setNo(int no) {
         this.no = no;
     }
+    public COLOR getColor() throws WrongColorException{
+        if (no == maxNumber())
+            return COLOR.FAKE_JOKER;
+        int colorNo = no / TILE_PER_COLOR;
+
+        return switch (colorNo) {
+            case 0 -> COLOR.YELLOW;
+            case 1 -> COLOR.BLUE;
+            case 2 -> COLOR.BLACK;
+            case 3 -> COLOR.RED;
+            default -> throw new WrongColorException(no);
+        };
+    }
+    public int getValue(){
+        return no % TILE_PER_COLOR + 1;
+    }
     public String toString(){
         try {
-            return "No: " + no + " Color: " + getColor(no).toString() + " Value: " + getValue();
+            return "No: " + no + " Color: " + getColor().toString() + " Value: " + getValue();
         } catch (WrongColorException e) {
             return "No: " + no + " Color: " + "UNKNOWN" + " Value: " + getValue();
         }
