0b41566bfce7f24379d877917ee4a1c56ee0951a
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-29 12:31:10 +0300

    Shuffle Mechanism Test

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,10 +1,18 @@
 /**
- * This is the launcher of our Rummikub Game.
+ * This is the launcher of our Okey Game.
  * @author Denizhan Soydas
  * @version 1.0-SNAPSHOT
  */
 public class Tester {
     public static void main(String[] args) {
-
+        Game game = new Game();
+        Integer[] tiles = game.getTiles();
+        for(Integer i: tiles)
+            System.out.print(i + " ");
+        System.out.println();
+        game.shuffle();
+        System.out.println("Shuffled.");
+        for(Integer i: tiles)
+            System.out.print(i + " ");
     }
 }
