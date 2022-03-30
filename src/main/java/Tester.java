03d1456503b58744e85b5c0729c4d47c80058112
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-03-30 12:38:07 +0300

    Unnecessary test lines removed.

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -9,7 +9,5 @@ public class Tester {
     public static void main(String[] args) {
         Game game = new Game();
         ArrayList<Tile> shuffledTiles = game.getShuffledTiles();
-        for(Tile i: shuffledTiles)
-            System.out.println(i + " ");
     }
 }
