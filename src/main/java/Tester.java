42cd712b7cc6d39a12d7e0d6302ae03199fd3f2c
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-04-03 13:11:31 +0300

    Version Changed: 1.2

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 /**
  * This is the launcher of our Okey Game.
  * @author Denizhan Soydas
- * @version 1.1
+ * @version 1.2
  */
 public class Tester {
     public static void main(String[] args) {
